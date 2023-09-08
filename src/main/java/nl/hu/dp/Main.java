d807e81bdd758c76bfee9e388dbc9b427dfba18d
Aanvil7 <svengroot69@gmail.com>
2023-09-08 15:54:15 +0200

    p2

--- src/main/java/nl/hu/dp/Main.java
+++ src/main/java/nl/hu/dp/Main.java
@@ -1,15 +1,29 @@
 package nl.hu.dp;
 
 
+import classes.Reiziger;
+import classes.ReizigerDAO;
+import classes.ReizigerDAOPsql;
+
 import java.sql.*;
+import java.util.List;
 
 
 public class Main {
     private static Connection connection;
+    /**
+     * P2. Reiziger DAO: persistentie van een klasse
+     * <p>
+     * Deze methode test de CRUD-functionaliteit van de Reiziger DAO
+     *
+     * @throws SQLException
+     */
 
     public static void main(String[] args) throws SQLException {
+        getConnection();
         System.out.println("Hello world!");
-        testConnection();
+        ReizigerDAO rdao = new ReizigerDAOPsql(connection);
+        testReizigerDAO(rdao);
     }
     private static Connection getConnection() throws SQLException {
         if (connection == null) {
@@ -27,19 +41,27 @@ public class Main {
             connection = null;
         }
     }
+    private static void testReizigerDAO(ReizigerDAO rdao) throws SQLException {
 
-    private static void testConnection() throws SQLException {
-        getConnection();
-        String query = "SELECT * FROM reiziger;";
-        PreparedStatement statement = connection.prepareStatement(query);
-        ResultSet set = statement.executeQuery();
-        while (set != null && set.next()) {
-            System.out.println(set.getString("achternaam"));
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
         }
+        System.out.println();
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
         closeConnection();
     }
-
-
-
-
 }
